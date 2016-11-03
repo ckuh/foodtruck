eb51d76f64e7c604d3fd0ad9d9445341059f7c96
ckuh <kuh_chris@yahoo.com>
2016-11-03 01:29:00 -0700

    [fix] filter data

--- server/controllers/foodTruck.js
+++ server/controllers/foodTruck.js
@@ -6,7 +6,9 @@ exports.get = (req, res) => {
     .then(data => {
       data = _.map(data, item => (
         _.pick(item, 'address', 'applicant', 'dayshours', 'facilitytype', 'fooditems', 'location')
-      ))
+      )).filter(item => {
+        return !!item.address && !!item.applicant && !!item.dayshours && !!item.facilitytype && !!item.fooditems && !!item.location
+      })
       res.status(200).send(data)
     })
     .catch(err => {
