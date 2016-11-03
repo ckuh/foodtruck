c0e20b0c0ea0278861976bb948940f6cbc3b8188
ckuh <kuh_chris@yahoo.com>
2016-11-03 00:24:05 -0700

    [fix] cleaned data sent to clinet

--- server/controllers/foodTruck.js
+++ server/controllers/foodTruck.js
@@ -1,8 +1,12 @@
 const model = require('../models/foodTruck.js')
+const _ = require('underscore')
 
 exports.get = (req, res) => {
   model.get()
     .then(data => {
+      data = _.map(data, item => (
+        _.pick(item, 'address', 'applicant', 'dayshours', 'facilitytype', 'fooditems', 'location')
+      ))
       res.status(200).send(data)
     })
     .catch(err => {
